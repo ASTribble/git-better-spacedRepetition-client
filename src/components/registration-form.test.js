a5f89e51e22d30b8faa362e4ff5746f92666399a
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-03-16 10:42:59 -0700

    Add inital registration form component test

---
+++ src/components/registration-form.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+
+import RegistrationForm from './registration-form';
+
+describe('<RegistrationForm />', () => {
+  it('Passes smoke test', () => {
+    shallow(<Registration />)
+  })
+})
