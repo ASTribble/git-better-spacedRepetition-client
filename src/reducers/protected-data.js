eaea655f416fc8be9cd41746581c4ac4cd5cbd53
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-16 11:25:38 -0700

    removed unused code from reducers/ protected-data.js and cleaned up some formatting

--- src/reducers/protected-data.js
+++ src/reducers/protected-data.js
@@ -3,9 +3,9 @@ import {
     FETCH_PROTECTED_DATA_ERROR,
     SAVE_QUESTION_RESULT_SUCCESS,
     SAVE_QUESTION_RESULT_ERROR,
-    FETCH_NEXT_QUESTION_SUCCESS,
-    FETCH_NEXT_QUESTION_ERROR
+    FETCH_NEXT_QUESTION_SUCCESS
 } from '../actions/protected-data';
+
 
 const initialState = {
     data: '',
@@ -15,54 +15,50 @@ const initialState = {
 };
 
 
-const feedBack = (result, answer) => {
-  console.log('the result was ', result, ' and the answer was ', answer)
-  let feedback;
-  if(answer) feedback = `Good job!`
-  else feedback = `Sorry the correct answer was: ${answer}`
-  return feedback;
-}
-
 export default function reducer(state = initialState, action) {
-    if (action.type === FETCH_PROTECTED_DATA_SUCCESS) {
-        return Object.assign({}, state, {
-            data: action.data,
-            error: null,
-            previousQuestionAnsweredCorrectly: null
-        });
-    } else if (action.type === FETCH_PROTECTED_DATA_ERROR) {
-        return Object.assign({}, state, {
-            error: action.error, 
-            previousQuestionAnsweredCorrectly: null
-        });
-    }
-    else if(action.type === SAVE_QUESTION_RESULT_SUCCESS){
-      console.log('state.data in saveQuestionResult:', state.data);
-      if(action.previousQuestionAnsweredCorrectly){
-        return Object.assign({}, state, {
-          data: {...state.data, timesAsked: state.data.timesAsked + 1, correct: state.data.timesAsked + 1},
-          savingQuestion: false,
-          previousQuestionAnsweredCorrectly: action.previousQuestionAnsweredCorrectly
-        });
-      }
-      else{
-        return Object.assign({}, state, {
-          data: {...state.data, timesAsked: state.data.timesAsked + 1},
-          savingQuestion: false,
-          previousQuestionAnsweredCorrectly: action.previousQuestionAnsweredCorrectly
-        });
-    }
-    } else if (action.type === SAVE_QUESTION_RESULT_ERROR){
+  if (action.type === FETCH_PROTECTED_DATA_SUCCESS) {
       return Object.assign({}, state, {
-        saveQuestion: false,
-        error: action.error,
-        previousQuestionAnsweredCorrectly: null
+          data: action.data,
+          error: null,
+          previousQuestionAnsweredCorrectly: null
       });
-    } else if (action.type === FETCH_NEXT_QUESTION_SUCCESS){
+  } 
+  else if (action.type === FETCH_PROTECTED_DATA_ERROR) {
       return Object.assign({}, state, {
-        previousQuestionAnsweredCorrectly: null
-      })
+          error: action.error, 
+          previousQuestionAnsweredCorrectly: null
+      });
+  }
+  else if(action.type === SAVE_QUESTION_RESULT_SUCCESS){
+  
+    if(action.previousQuestionAnsweredCorrectly){
+      return Object.assign({}, state, {
+        data: {...state.data, timesAsked: state.data.timesAsked + 1, correct: state.data.timesAsked + 1},
+        savingQuestion: false,
+        previousQuestionAnsweredCorrectly: action.previousQuestionAnsweredCorrectly
+      });
     }
-    return state;
+    else{
+      return Object.assign({}, state, {
+        data: {...state.data, timesAsked: state.data.timesAsked + 1},
+        savingQuestion: false,
+        previousQuestionAnsweredCorrectly: action.previousQuestionAnsweredCorrectly
+      });
+    }
+  } 
+  else if (action.type === SAVE_QUESTION_RESULT_ERROR){
+    return Object.assign({}, state, {
+      saveQuestion: false,
+      error: action.error,
+      previousQuestionAnsweredCorrectly: null
+    });
+  } 
+  else if (action.type === FETCH_NEXT_QUESTION_SUCCESS){
+    return Object.assign({}, state, {
+      previousQuestionAnsweredCorrectly: null
+    });
+  }
+
+  return state;
 }
 
