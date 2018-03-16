7cbec534d799f829309cfa8ffcaa474a781b1347
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-16 10:05:48 -0700

    made some adjustments to feedback

--- src/components/question-form.js
+++ src/components/question-form.js
@@ -5,6 +5,7 @@ import {required, nonEmpty} from '../validators';
 import {connect} from 'react-redux';
 import { saveQuestionResult, fetchNextQuestion } from '../actions/protected-data';
 import Tracker from './tracker';
+import './question-form.css';
 /* eslint-disable*/ 
 export class QuestionForm extends React.Component {
     onSubmit(values){
@@ -34,7 +35,7 @@ export class QuestionForm extends React.Component {
                 <div className='question-text'>
                 <h2>{this.props.text}</h2>
                 </div>
-                {feedback !== null ? feedback ? <p className='feedback'>Correct!</p> :<div> <p className='feedback'>Incorrect.</p> <p className='answer'>The correct answer was: {this.props.answer}</p></div> : ''}
+                {feedback !== null ? feedback ? <div className='feedback-div'> <p className='feedback'>Correct!</p> </div>:<div className='feedback-div'> <p className='feedback'>Incorrect.</p> <p className='answer'>The correct answer was: {this.props.answer}</p></div> : ''}
                 <Field
                     component={Input}
                     type='text'
